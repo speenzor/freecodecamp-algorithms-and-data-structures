bfc92f52f5c52b3a61c22727125cef4822d8d764
Spencer Corwin <spencercorwin@mac.com>
2018-11-05 22:51:54 -0800

    Update index.js

--- front-end-libraries-projects/random-quote-machine/index.js
+++ front-end-libraries-projects/random-quote-machine/index.js
@@ -1,45 +1,57 @@
 // You may add additional external JS and CSS as needed to complete the project, however the current external resource MUST remain in place for the tests to work. BABEL must also be left in place. PLEASE NOTE: Adding global style rules using the * selector, or by adding rules to body {..} or html {..}, or to all elements within body or html, i.e. h1 {..}, has the potential to pollute the test suite's CSS. Try adding: * { color: red }, for a quick example!
 
-let quotesData;
-let randomNum = Math.floor(Math.random()*102);
-(() => {
-  const xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      quotesData = JSON.parse(this.responseText);
-      document.getElementById("quote").innerHTML = quotesData.quotes[randomNum].quote;
-      document.getElementById("testauthor").innerHTML = quotesData.quotes[randomNum].author;
-    }
-  };
-  xhttp.open('GET', 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json', true);
-  xhttp.send();
-})();
-
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      quotes: [{quote: 'This is Sparta!', author: 'me'}]
+      quotes: [{quote: 'Loading quotes...', author: ''}],
+      randomNum: 0,
+      prevNum: 0
     }
     this.handleNewQuote = this.handleNewQuote.bind(this);
-    this.handleTweet = this.handleTweet.bind(this);
+    this.handlePrev = this.handlePrev.bind(this);
+  }
+  
+  componentDidMount() {
+    const URL = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json';
+    const fetchQuotes = ((url) => {
+  return fetch(url)
+    .then(response => response.json());
+})(URL)
+    .then(response => {this.setState({
+      quotes: response.quotes,
+      randomNum: Math.floor(Math.random()*102)
+    })});
   }
   
   handleNewQuote() {
-    
+    this.setState({
+      randomNum: Math.floor(Math.random()*102),
+      prevNum: this.state.randomNum
+    })
   }
   
-  handleTweet() {
-    
+  handlePrev() {
+    this.setState({
+      randomNum: this.state.prevNum
+    })
   }
 
   render() {
     return (
-      <div>
-        <p id="text">{this.state.quotes[0].quote}</p>
-        <p id="author">{this.state.quotes[0].author}</p>
-        <New func={this.handleNewQuote}/>
-        <Tweet func={this.handleTweet}/>
+      <div id="container">
+      <div id="quote-box">
+        <div id="quote">
+          <p id="text">{this.state.quotes[this.state.randomNum].quote}</p>
+          <p id="author">{'-'+this.state.quotes[this.state.randomNum].author}</p>
+        </div>
+        <div id="buttons">
+          <New func={this.handleNewQuote}/>
+          <Prev func={this.handlePrev}/>
+          <Tweet currentQuote={'"' + this.state.quotes[this.state.randomNum].quote + '" -' + this.state.quotes[this.state.randomNum].author}/>
+        </div>
+      </div>
+        <p id="attribution">by <a href="http://spencercorwin.com" target="_blank">Spencer Corwin</a></p>
       </div>
     );
   }
@@ -48,18 +60,27 @@ class App extends React.Component {
 class New extends React.Component {
   render() {
     return (
-      <a onClick={this.props.func} id="new-quote">New Quote</a>
+      <button onClick={this.props.func} id="new-quote">New Quote</button>
     )
   }
 }
 
 class Tweet extends React.Component {
   render() {
+    let tweetLink = 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + this.props.currentQuote;
     return (
-      <a href="twitter.com/intent/tweet" id="tweet-quote">Tweet Quote</a>
+      <a href={tweetLink} target="_blank" id="tweet-quote">Tweet Quote</a>
+    )
+  }
+}
+
+class Prev extends React.Component {
+  render() {
+    return (
+      <button onClick={this.props.func} id="prev-quote">Previous Quote</button>
     )
   }
 }
 
 
-ReactDOM.render(<App />, document.getElementById('quote-box'));
+ReactDOM.render(<App />, document.getElementById("app"));

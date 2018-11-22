ad0e3119b7be472070688dac67b689e94c1afc89
Spencer Corwin <spencercorwin@mac.com>
2018-11-21 22:15:27 -0800

    Adds back service worker file and index.js

--- front-end-libraries-projects/pomodoro-clock/src/index.js
+++ front-end-libraries-projects/pomodoro-clock/src/index.js
@@ -225,4 +225,9 @@ class Session extends React.Component {
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("app"))
+ReactDOM.render(<App />, document.getElementById("app"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();

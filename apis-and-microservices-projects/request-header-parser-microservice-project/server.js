77709b0cc04d5e70ea6efb811e51360125c3ddb2
Spencer Corwin <spencercorwin@mac.com>
2018-12-05 22:19:48 -0800

    Adds files for request header microservice project and adds back gitconfig files

---
+++ apis-and-microservices-projects/request-header-parser-microservice-project/server.js
@@ -0,0 +1,39 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+// your first API endpoint... 
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+//API endpoint to get and return request headers
+app.get('/api/whoami', (req, res) => {
+  const headers = req.headers;
+  const ipaddress = headers['x-forwarded-for'].slice(0, headers['x-forwarded-for'].search(/,/));
+  const language = headers['accept-language'];
+  const software = headers['user-agent'];
+  res.json({'ipaddress': ipaddress, 'language': language, 'software': software});
+});
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});

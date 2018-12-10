642400f3158406b757877d19a2ff28a9b0487705
Spencer Corwin <spencercorwin@mac.com>
2018-12-10 13:55:28 -0800

    Fixes final code for exercise tracker project

--- apis-and-microservices-projects/exercise-tracker-project/server.js
+++ apis-and-microservices-projects/exercise-tracker-project/server.js
@@ -29,26 +29,68 @@ const Schema = mongoose.Schema;
 const exerciseSchema = new Schema ({
   description: {type: String, required: true},
   duration: {type: Number, required: true},
-  date: {type: Date, default: Date.now}
+  date: {type: String}
 });
 const userSchema = new Schema ({
   username: {type: String, required: true},
-  count: Number,
-  log: [exerciseSchema]
+  log: []
 });
 const Model = mongoose.model('Model', userSchema);
 
 
 //GET user's exercise log
 app.get('/api/exercise/log', (req, res) => {
-  const userId = req.query.userId; //required
-  const from = req.query.from; //optional
-  const to = req.query.to; //optional
-  const limit = req.query.limit; //optional
+  const userId = req.query.userId;
   
-  //READ: check for userId first. If doesn't exist then return something
-  //Return is logic based on from, to, and limit parameters
-  //Returns: {"_id":"By9gIsu14","username":"scorwin","count":1,"log":[{"description":"4","duration":34,"date":"Sat Dec 08 2018"}]}
+  //Setup defaults for from and to variables
+  let from = req.query.from;
+  let to = req.query.to;
+  const getTime = (query) => new Date(query).getTime();
+  if (isNaN(getTime(from))) {
+    from = 0;
+  } else {
+    from = getTime(from);
+  };
+  if (isNaN(getTime(to))) {
+    to = getTime('9000');
+  } else {
+    to = getTime(to);
+  };
+  
+  //Setup defaults for limit variable
+  let limit = req.query.limit; //optional
+  //Set limit to empty string if limit input is NaN or is empty
+  if (limit === '' || isNaN(Number(limit))) {
+    limit = '';
+  } else {
+    limit = Number(limit);
+  };
+
+  //READ: check for userId first
+  Model.findById({_id: userId}, (err, data) => {
+    console.log('Checking database for userId.');
+    console.log(data);
+    if (data == false || data == undefined) {
+      //If data is an empty array then the userId doesn't exist
+      console.log('userId does not exist.');
+      res.json('This userId does not exist.');
+    } else {
+      //Return data with log based on from, to, limit parameters
+      const rawLog = data.log;
+      const filteredLog = rawLog.filter(entry => new Date(entry.date).getTime() >= from && new Date(entry.date).getTime() <= to);
+      let returnLog = [];
+      if (limit !== '') {
+        if (filteredLog.length <= limit) {
+          returnLog = filteredLog;
+        } else {
+          returnLog = filteredLog.slice(0, limit);
+        };
+      } else {
+        returnLog = filteredLog;
+      };
+      res.json({_id: data._id, username: data.username, count: returnLog.length, log: returnLog});
+    }
+  });
 });
 
 //New user POST
@@ -57,26 +99,27 @@ app.post('/api/exercise/new-user', (req, res) => {
   //READ: Check if username exists in database
   Model.find({username: username}, (err, data) => {
     console.log('Checking database if new username already exists.');
-    if (data) {
+    console.log(data);
+    //If it returns an empty array then the username is not taken and should evaluate to false
+    if (data == true) {
       //If username already exists then return message
       console.log('Username already exists.');
-      return "Username already taken. Please try another username."
+      return res.json("Username already taken. Please try another username.");
     } else {
       //CREATE: If username doesn't exist then create new username
       console.log('New username created and added to database.');
       const newUser = new Model({
         username: username,
-        count: 0,
         log: []
       });
       newUser.save((err, data) => {
-        //NEED TO GET THE USER ID FROM data to add to json response below
         if (err) {
-          console.log(''); //ADD HEREEEEEEEEEEE
+          console.log('Error in saving new user.');
         };
-        console.log(''); //ADD HEREEEEEEEEEEE
+        console.log('New user succesfully saved and returned:');
+        console.log('Data: ' + data);
+        return res.json({'username': data.username, '_id': data._id});
       });
-      return res.json({'username': username, '_id': ''}); //ADD HEREEEEEEEEEEE
     }
     if (err) {
       console.log('Error in creating new user: ' + err);
@@ -89,27 +132,45 @@ app.post('/api/exercise/new-user', (req, res) => {
 app.post('/api/exercise/add', (req, res) => {
   const userId = req.body.userId;
   const description = req.body.description;
-  const duration = req.body.duration;
-  const date = req.body.date;
+  const duration = Number(req.body.duration);
+  let date = req.body.date;
+  //If date is empty then set to today's date
+  if (date === '') {
+    date = new Date(Date.now()).toUTCString().slice(0,16);
+  } else {
+    date = new Date(date).toUTCString().slice(0,16);
+  };
   
-  //Find and update by _id
-  //First check if userId exists
-  //Then check if description is filled out
-  //Then check if duration is filled out
-  //Then check if duration is correct format (number)
-  //If date is filled out then check if correct format
-  //Require userId, description, duration
-  //Check database for record by _id
-  //If id does not exist then return something "That userId does not exist. Go back to
-  //homepage to create a new username and get a unique userId
-  //If id does exist then add and return {'username': '', 'description': '', 'duration', '_id': '', 'date': if no input then current date "Sat Dec 08 2018"}
+  if (description == false) {
+    //Check if description is filled out
+    res.json('New exercise requires a description.');
+  } else if (duration == false) {
+    //Check if duration is filled out
+    res.json('New exercise requires a duration.');
+  } else if (Number.isNaN(duration)) {
+    //Check if duration is a number
+    res.json('Duration needs to be a number.');
+  } else if (new Date(date).toUTCString() === 'Invalid Date') {
+    //Check if date is correct format
+    res.json('Date needs to be in correct format.');
+  } else {
+    //Find and update by _id
+    const newExercise = {
+          description: description,
+          duration: duration,
+          date: date
+        };
+    Model.findByIdAndUpdate({_id: userId}, {$push: {log: newExercise}}, {new: true}, (err, data) => {
+      console.log('New exercise pushed to log of user: ' + data.username);
+      res.json({username: data.username, description: description, duration: duration, _id: data._id, date: date});
+    });
+  };
 });
-
 
 // Not found middleware
 app.use((req, res, next) => {
   return next({status: 404, message: 'not found'})
-})
+});
 
 // Error-handling middleware
 app.use((err, req, res, next) => {
@@ -128,9 +189,9 @@ app.use((err, req, res, next) => {
   }
   res.status(errCode).type('txt')
     .send(errMessage)
-})
+});
 
 //Setup port configuration and listen
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
-})
+});
